b3bf9d0e96efefc9b3a05cb3a6ed86028c897bfa
Pouya <zarepouya7@gmail.com>
2023-03-24 12:50:51 +0200

    add tasks list and add get api to project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,33 @@
 import "./App.css";
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 function App() {
-  const [title, settitle] = useState('')
+  const [title, setTitle] = useState('');
+  const [tasks, settasks] = useState([])
+  const handleSubmit = async(e: React.FormEvent) => {
+    e.preventDefault();
+    await fetch("http://localhost:5006/tasks", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title }),
+    });
+    setTitle('');
+  }
+  useEffect(() => {
+    async function fetchData() {
+      const tasks =  await fetch('http://localhost:5006/tasks');
+      const response = await tasks.json()
+      settasks(response)
+    }
+    fetchData();
+  }, [])
+  
+
   return (
     <>
-    <div className="">
       <div className="max-w-lg mx-auto items-center mt-10 w-full h-full ">
-        <form className="bg-slate-50 shadow-md rounded p-10 mb-4">
+        <form className="bg-slate-50 shadow-md rounded p-10 mb-4" onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
               className="block text-cyan-800 text-xl text-center font-light mb-2"
@@ -16,25 +36,31 @@ function App() {
               Add New Task
             </label>
             <input
-              className="shadow appearance-none border p-2 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border p-2 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="task"
-              onChange={(e:React.ChangeEvent<HTMLInputElement>) => {
-                settitle(e.target.value)
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setTitle(e.currentTarget.value)
               }}
               type="text"
               placeholder="Write your Task"
+              value={title}
             />
           </div>
           <div className="flex items-center justify-between">
             <button
               className="bg-cyan-500 hover:bg-cyan-800 w-full transition-all duration-300 text-white font-bold py-2 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+              type="submit"
+              disabled={!title}
             >
               Add Task
             </button>
           </div>
         </form>
       </div>
+      <div className="w-8/12 mx-auto p-10 flex justify-between text-center align-middle flex-wrap">
+        {tasks.map((item) => (
+          <div className=" bg-slate-50 shadow-sm p-10 m-2 text-center w-2/12 rounded-sm">{item.title}</div>
+        ))}
       </div>
     </>
   );
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,43 @@
 import { config } from "dotenv";
 config();
-import express, {Request,Response} from 'express'
-import Task from './models/Task'
+
+import express, { Request, Response } from 'express';
+import Task from './models/Task';
+import cors from 'cors';
 import mongoose from 'mongoose';
 
-
 const app = express();
-//middleware  function 
-app.use(express.json())
-const PORT = 5006
-app.post('/tasks',async(req:Request,res:Response) => {
+
+// cors
+app.use(cors({
+    origin: "*"
+}));
+
+// middleware function 
+app.use(express.json());
+
+const PORT = 5006;
+
+app.post('/tasks', async (req: Request, res: Response) => {
     const newTask = new Task({
         title: req.body.title
     });
-    //save to database
-   const createTask = await newTask.save();
-   res.json(createTask);
+    
+    // save to database
+    const createTask = await newTask.save();
+    res.json(createTask);
 });
+
+app.get('/tasks', async (req: Request, res: Response) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+
 mongoose.connect(process.env.DB_URL!)
-.then(() => {
-    console.log(`connected to the database ${PORT}`)
-    app.listen(PORT);
-})
+    .then(() => {
+        console.log(`connected to the database on port ${PORT}`);
+        app.listen(PORT);
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
